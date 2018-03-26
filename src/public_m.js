26eae6093f2acdea135a88087ee37256211304c9
王鑫泽 <88923616@qq.com>
2018-03-26 09:49:54 +0800

    no message

--- src/public_m.js
+++ src/public_m.js
@@ -7,8 +7,8 @@ var sd_erttx = {}
 alert($cookie.get('token'));*/
 sd_erttx.user_uuid = $cookie.get('user_uuid');
 sd_erttx.token = $cookie.get('token')
-// sd_erttx.user_uuid = '39d1e6e7-a527-4a44-a475-9adb2dd52b10';
-// sd_erttx.token = 'c5f8f34e-6906-4e9f-ac7d-d8883c3828f3';
+/*sd_erttx.user_uuid = '39d1e6e7-a527-4a44-a475-9adb2dd52b10';
+sd_erttx.token = 'c5f8f34e-6906-4e9f-ac7d-d8883c3828f3'*/
 export default {
   install(Vue, options) {
     Vue.prototype.Title = function (title) {
